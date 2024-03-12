e2ac1188970d4bfaeaa2cd68bd3c5eabff3fdd25
eldmar <ridkous@gmail.com>
2024-03-12 13:22:14 +0000

    change current date

--- script.js
+++ script.js
@@ -5,12 +5,22 @@ const body = document.querySelector("body");
 const message = document.querySelector(".message");
 
 const item = localStorage.getItem("time");
+const savedButtonState = localStorage.getItem("buttonState");
+const savedBackgroundColor = localStorage.getItem("backgroundColor");
 
 // Дізнаємось поточну дату
 
 function currentTime() {
   const time = new Date();
-  const correctTime = `${time.toLocaleDateString()} ${time.toLocaleTimeString()}`;
+  const day = time.getDate();
+  const month = time.getMonth();
+  const year = time.getFullYear();
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const correctTime = `${day}-${
+    month + 1
+  }-${year} ${hours}:${minutes}:${seconds}`;
   return correctTime;
 }
 
@@ -31,10 +41,13 @@ button.addEventListener("click", () => {
   }
 
   localStorage.setItem("time", message.textContent);
+  localStorage.setItem("buttonState", button.textContent);
+  localStorage.setItem("backgroundColor", document.body.style.backgroundColor);
 });
 
 // Виводимо зі стореджу останній час виконання події
 
 if (item !== null) {
   message.textContent = `${item}`;
+  document.body.style.backgroundColor = savedBackgroundColor;
 }
