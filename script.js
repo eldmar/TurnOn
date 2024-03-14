e1ec7cbb0df5a2fcaeb4f5790e2d93dfa1c6b102
eldmar <ridkous@gmail.com>
2024-03-14 15:45:17 +0000

    add theme to storage

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const button = document.querySelector("button");
 const body = document.querySelector("body");
 const message = document.querySelector(".message");
 
-const item = localStorage.getItem("time");
-const savedButtonState = localStorage.getItem("buttonState");
-
+const timeStorage = localStorage.getItem("time");
+const currentTheme = localStorage.getItem("lightTheme");
+let lightTheme;
 // Функія додавання 0 , якщо дата менше 10
 
 function addZero(number) {
-  return number < 10 ? "0" + number : number;
+  return number < 10 ? `0${number}` : `${number}`;
 }
 
 // Дізнаємось поточну дату
 
-function currentTime() {
+function getCurrentTime() {
   const time = new Date();
   const day = addZero(time.getDate());
   const month = addZero(time.getMonth() + 1);
@@ -35,29 +35,30 @@ button.addEventListener("click", () => {
   message.classList.toggle("message-active");
 
   const currentText = button.textContent;
+
   if (currentText === "Turn off") {
     button.textContent = "Turn on";
-    message.textContent = `Last turn off: ${currentTime()}`;
+    message.textContent = `Last turn off: ${getCurrentTime()}`;
+    lightTheme = false;
   } else {
     button.textContent = "Turn off";
-    message.textContent = `Last turn on: ${currentTime()}`;
+    message.textContent = `Last turn on: ${getCurrentTime()}`;
+    lightTheme = true;
   }
 
-  localStorage.setItem("time", message.textContent);
-  localStorage.setItem("buttonState", button.textContent);
+  localStorage.setItem("time", getCurrentTime());
+  localStorage.setItem("lightTheme", lightTheme);
 });
 
 // Виводимо зі стореджу останній час виконання події
-
-if (item !== null) {
-  message.textContent = `${item}`;
-
-  if (savedButtonState === "Turn off") {
+if (timeStorage !== null) {
+  if (currentTheme === "true") {
+    message.textContent = `Last turn on: ${timeStorage} `;
+  } else {
+    message.textContent = `Last turn off: ${timeStorage} `;
     body.classList.add("active-body");
-    button.classList.toggle("active-button");
+    button.classList.add("active-button");
     message.classList.add("message-active");
     button.textContent = "Turn on";
   }
 }
-
-console.log(savedButtonState);
